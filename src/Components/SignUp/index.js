7c17f356017858f71c268a85a31aba1035bc3f47
Carlo Bruno <carlofbruno@gmail.com>
2019-07-15 18:41:54 -0700

    bug fix SignUp

--- src/Components/SignUp/index.js
+++ src/Components/SignUp/index.js
@@ -30,7 +30,7 @@ class SignUpFormBase extends Component {
     const { username, email, passwordOne } = this.state;
 
     this.props.firebase
-      .doCreteUserWithEmailAndPassword(email, passwordOne)
+      .doCreateUserWithEmailAndPassword(email, passwordOne)
       .then((authUser) => {
         this.setState({ ...INITIAL_STATE });
         this.props.history.push(ROUTES.HOME);
